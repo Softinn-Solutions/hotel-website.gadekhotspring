0e4a27474cf037f44ac2e2272e274a72dd73f871
Mohamad Asyraf Osman <asyraf@mysoftinn.com>
2020-03-18 10:37:02 +0800

    amend according to the review

---
+++ EmbunLuxuryVillas/ViewModels/SendTourPackageInquiryMailViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmbunLuxuryVillas.ViewModels
+{
+    public class SendTourPackageInquiryMailViewModel
+    {
+        public string Package { get; set; }
+        public string TravelDate { get; set; }
+        public string Name { get; set; }
+        public string CompanyName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Message { get; set; }
+    }
+}
