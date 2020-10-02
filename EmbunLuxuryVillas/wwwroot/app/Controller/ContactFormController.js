d17d540bc66860c44dfd4c96f386cd0d9dd0943b
HO LAI MING <laiming@mysoftinn.com>
2020-10-02 12:18:48 +0800

    Add loading indicator.

--- EmbunLuxuryVillas/wwwroot/app/Controller/ContactFormController.js
+++ EmbunLuxuryVillas/wwwroot/app/Controller/ContactFormController.js
@@ -1,4 +1,4 @@
-﻿var contactFormController = ["$scope","$http",function ($scope, $http) {
+﻿var contactFormController = ["$scope", "$http", function ($scope, $http) {
     $scope.form = {
         Salutation: "Mr",
         Name: "",
@@ -8,11 +8,16 @@
         Message: ""
     };
 
+    $scope.isLoading = false;
+
     $scope.onContactFormSubmit = function () {
+        $scope.isLoading = true;
         $http.post("/Home/SendEmail", $scope.form).then(function successCallback(response) {
+            $scope.isLoading = false;
             swal("Email sent!", response.statusText, "success");
             contactForm.reset();
         }, function errorCallback(response) {
+            $scope.isLoading = false;
             swal("Oops!", response.statusText, "error");
         });
     }
